bf63210b18f297377db42adbde3a1386581c7f92
Mufassir-Chowdhury <mac22214u@gmail.com>
2023-05-09 11:49:45 +0600

    removed data from web.xml

--- src/main/java/cse/web/ApproveInstructorController.java
+++ src/main/java/cse/web/ApproveInstructorController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ApproveInstructorController
  */
-@WebServlet("/ApproveInstructorController")
+@WebServlet("/ApproveInstructor")
 public class ApproveInstructorController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
--- src/main/java/cse/web/ApproveStudentController.java
+++ src/main/java/cse/web/ApproveStudentController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ApproveStudentController
  */
-@WebServlet("/ApproveStudentController")
+@WebServlet("/ApproveStudent")
 public class ApproveStudentController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
